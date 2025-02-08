2ca426ba5522ed50e10bf37306ee777149f474b0
Jared Pochtar <jpochtar@gmail.com>
2025-02-08 00:54:14 -0500

    testing unrelated process.env thing

--- src/pages/api/crash-simple.ts
+++ src/pages/api/crash-simple.ts
@@ -2,7 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  throw new Error("Sentry Example API Route Error");
+  console.log(process.env);
+
+  // throw new Error("Sentry Example API Route Error");
   // biome-ignore lint/correctness/noUnreachable: <explanation>
   res.status(200).json({ name: "John Doe" });
 }
